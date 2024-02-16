ab0fc7a8056aea194a591760e8ca78e57f52a7f7
C Grogan <conorgrogan23@gmail.com>
2024-02-16 16:59:42 +0000

    added array of questions

--- index.js
+++ index.js
@@ -1,12 +1,35 @@
-// packages required for the application to run
+// Packages required for the application to run
 const fs = require("fs");
 const path = require('path');
 const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
 
-// array of questions for user
-const questions = [
+// Array of questions for user
+const questions = [ 
+  {
+    type: "input",
+    name: "title",
+    message: "What will you name your project?",
+  },
+  
+  {
+    type: "input",
+    name: "description",
+    message: "Provide a brief description of the functionality and purpose of the project",
+  },
 
+  {
+    type: "input",
+    name: "license",
+    message: "Select a license to be used for this project",
+    licenses: ["MIT", "Apache 2.0", "ISC", "BSD 3-Clause", "BSD 2-Clause", "None"],
+  },
+
+  {
+    type: "input",
+    name: "technologies",
+    message: "State the technologies or languages being used for this project",
+  },
 ];
 
 // function to write README file
